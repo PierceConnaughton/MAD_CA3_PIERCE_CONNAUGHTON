fe6c5d6877c012cab0465b4baddb8a5d5e41956b
Ti43652 <47326833+Ti43652@users.noreply.github.com>
2020-12-13 13:29:45 +0000

    Got the sequence to work

--- app/src/main/java/edu/pconnaughton/ca3/MainActivity.java
+++ app/src/main/java/edu/pconnaughton/ca3/MainActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnOne, btnTwo, btnThree, btnFour;
     Handler handlerUI = new Handler();
+    int[] sequence = new int[4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         btnFour = findViewById(R.id.btnFourthColor);
     }
 
-    public void doPlay(View view) {
+    public void doPlay(View view) throws InterruptedException {
 
         int x;
 
@@ -32,27 +34,35 @@ public class MainActivity extends AppCompatActivity {
 
             x  = getRandomNumber();
 
+            sequence[i] = x;
+
+            System.out.println(sequence[i]);
+
             switch (x) {
                 case 1:
                     setBtnClicked(btnOne);
+
                     break;
 
                 case 2:
                     setBtnClicked(btnFour);
+
                     break;
 
                 case 3:
                     setBtnClicked(btnThree);
+
                     break;
 
                 case 4:
                     setBtnClicked(btnFour);
+
                     break;
             }
         }
 
-            final Intent gameStartActivity = new Intent(view.getContext(), GameStartActivity.class);
 
+            final Intent gameStartActivity = new Intent(view.getContext(), GameStartActivity.class);
 
         handlerUI.postDelayed(new Runnable() {
             @Override
@@ -65,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     public int getRandomNumber() {
-        int rndNum = (int) ( Math.random() * 2 + 1);
+        int rndNum = (int) ( Math.random() * 4 + 1);
         return rndNum;
     }
 
@@ -79,4 +89,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }, 1000);
     }
+
+
 }
