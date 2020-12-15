a5ab345424ec272101e8f1a6de9c892cda4d33fc
Ti43652 <47326833+Ti43652@users.noreply.github.com>
2020-12-15 23:58:55 +0000

    North and South Accelerometer working

--- app/src/main/java/edu/pconnaughton/ca3/GameStartActivity.java
+++ app/src/main/java/edu/pconnaughton/ca3/GameStartActivity.java
@@ -6,7 +6,9 @@ import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -22,6 +24,27 @@ public class GameStartActivity extends AppCompatActivity implements SensorEventL
     int[] usequence = new int[20];
 
     TextView tvRound;
+
+    private final double NORTH_MOVE_FORWARD = 9.0;     // upper mag limit
+    private final double NORTH_MOVE_BACKWARD = 7.0;      // lower mag limit
+
+    private final double SOUTH_MOVE_FORWARD = 7.0;     // upper mag limit
+    private final double SOUTH_MOVE_BACKWARD = 9.0; // lower mag limit
+
+    private final double NORTH_MOVE_FORWARD_MINUS = -7.0;     // upper mag limit
+    private final double NORTH_MOVE_BACKWARD_MINUS = -9.0;      // lower mag limit
+
+    private final double SOUTH_MOVE_FORWARD_MINUS = -9.0;     // upper mag limit
+    private final double SOUTH_MOVE_BACKWARD_MINUS = -7.0; // lower mag limit
+
+    boolean detectNorth = false;      // detect high limit
+    boolean detectSouth = false;      // detect high limit
+
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+
+    Handler handlerUI = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +83,10 @@ public class GameStartActivity extends AppCompatActivity implements SensorEventL
                 lastNum++;
             }
         }
+
+        // we are going to use the sensor service
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
 
     public void doBtnClick(View view) {
@@ -81,6 +108,151 @@ public class GameStartActivity extends AppCompatActivity implements SensorEventL
                 break;
         }
 
+        CheckUserValue(view);
+
+    }
+
+    protected void onResume() {
+        super.onResume();
+        // turn on the sensor
+        mSensorManager.registerListener(this, mSensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    /*
+     * App running but not on screen - in the background
+     */
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);    // turn off listener to save power
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+
+
+
+        final Button btnNorth = findViewById(R.id.btnFirstColor);
+        final Button btnSouth = findViewById(R.id.btnFourthColor);
+        Button btnEast = findViewById(R.id.btnThirdColor);
+        Button btnWest = findViewById(R.id.btnFourthColor);
+        // Can we get a north movement
+        // you need to do your own mag calculating
+
+        //screen rotated to the left
+        if(x > 0){
+            if ((x > NORTH_MOVE_FORWARD && z > 0) && (detectNorth == false)) {
+                detectNorth = true;
+            }
+            if ((x < NORTH_MOVE_BACKWARD && z > 0) && (detectNorth == true)) {
+                // we have a tilt to the north
+
+
+                userNum = 1;
+
+                btnNorth.setPressed(true);
+
+                handlerUI.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        btnNorth.setPressed(false);
+                    }
+                }, 1000);
+
+                CheckUserValue(btnNorth);
+
+                detectNorth = false;
+            }
+
+            if ((x < SOUTH_MOVE_FORWARD && z < 0) && (detectSouth == false)) {
+                detectSouth = true;
+            }
+            if ((x > SOUTH_MOVE_BACKWARD && z < 0) && (detectSouth == true)) {
+                // we have a tilt to the north
+                userNum = 4;
+
+                btnSouth.setPressed(true);
+
+                handlerUI.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        btnSouth.setPressed(false);
+                    }
+                }, 1000);
+
+                CheckUserValue(btnSouth);
+
+                detectSouth = false;
+            }
+        }
+
+        //screen rotated to the right
+        if( x < 0){
+            if ((x > NORTH_MOVE_FORWARD_MINUS && z > 0) && (detectNorth == false)) {
+                detectNorth = true;
+            }
+            if ((x < NORTH_MOVE_BACKWARD_MINUS && z > 0) && (detectNorth == true)) {
+                // we have a tilt to the north
+                userNum = 1;
+
+                btnNorth.setPressed(true);
+
+                handlerUI.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        btnNorth.setPressed(false);
+                    }
+                }, 1000);
+
+                CheckUserValue(btnNorth);
+
+                detectNorth = false;
+            }
+
+            if ((x < SOUTH_MOVE_FORWARD_MINUS && z < 0) && (detectSouth == false)) {
+                detectSouth = true;
+            }
+            if ((x > SOUTH_MOVE_BACKWARD_MINUS && z < 0) && (detectSouth == true)) {
+                // we have a tilt to the north
+                userNum = 4;
+
+                btnSouth.setPressed(true);
+
+                handlerUI.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        btnSouth.setPressed(false);
+                    }
+                }, 1000);
+
+                CheckUserValue(btnSouth);
+
+                detectSouth = false;
+            }
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // not used
+    }
+
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        long factor = (long) Math.pow(10, places);
+        value = value * factor;
+        long tmp = Math.round(value);
+        return (double) tmp / factor;
+    }
+
+    public void CheckUserValue(View view){
 
         if(userNum == usequence[sequenceNum]){
             Toast.makeText(this,"Correct",Toast.LENGTH_SHORT).show();
@@ -96,10 +268,10 @@ public class GameStartActivity extends AppCompatActivity implements SensorEventL
 
             gameOverStartActivity.putExtra("userRound", userRound);
 
-        startActivity(gameOverStartActivity);
+            startActivity(gameOverStartActivity);
 
             userScore = 0;
-        finish();
+            finish();
         }
 
         sequenceNum++;
@@ -135,17 +307,5 @@ public class GameStartActivity extends AppCompatActivity implements SensorEventL
             Toast.makeText(this,"Round Complete",Toast.LENGTH_SHORT).show();
             finish();
         }
-
-
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
     }
 }
